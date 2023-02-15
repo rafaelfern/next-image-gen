d1021c0044e22160b6df01ad679fdeff0f11bd84
rafaelfern <gdcmf777@gmail.com>
2023-02-15 10:01:01 -0300

    Clear icon button

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import styles from './styles.module.scss'
 import BtnForm from './BtnForm'
+import { AiOutlineClose } from 'react-icons/ai'
 
 export interface IProps {
   desiredImgInput: string;
@@ -10,19 +11,19 @@ export interface IProps {
 }
 
 export default function Index({ desiredImgInput, setDesiredImgInput, getImage, disabled }: IProps) {  
-  const inputRef = useRef(null)
-
   return (
     <div className={styles.formContent}>
-      <input
-        ref={inputRef}
-        type="text" 
-        id="textInput" 
-        value={desiredImgInput} 
-        onChange={e => setDesiredImgInput(e.target.value)}
-        placeholder='Input any idea of image you want'
-        data-testid="input-prompt"
-      />
+      <div className={styles.inputWrapper}>
+        <input
+          type="text" 
+          id="textInput" 
+          value={desiredImgInput} 
+          onChange={e => setDesiredImgInput(e.target.value)}
+          placeholder='Input any idea of image you want'
+          data-testid="input-prompt"
+        />
+        <AiOutlineClose className={styles.closeIcon} onClick={() => setDesiredImgInput('')}/>
+      </div>
       <BtnForm getImage={getImage} disabled={disabled}/>
     </div>
   )
